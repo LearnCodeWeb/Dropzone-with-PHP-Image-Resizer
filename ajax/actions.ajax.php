3e10e08e30c7e52b7a6a05c893acaebf2ffaab47
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2017-05-19 18:26:01 +0900

    Some extra functionality added
    Water Mark,
    Check Image Size.
    Water Mark Position set.

--- ajax/actions.ajax.php
+++ ajax/actions.ajax.php
@@ -5,6 +5,7 @@ ini_set('upload_max_filesize', '1000M');
 ini_set('memory_limit','1000M');*/
 
 require('../az.multi.upload.class.php');
-$rename	=	rand().time();
+$rename	=	rand(1000,5000).time();
 $upload	=	new ImageUploadAndResize();
-$upload->uploadFiles('files',$rename,'../uploads',0777,100,'');
+
+$upload->uploadFiles('files', '../uploads', 250, '../mini-logo.png', 20, 20, $rename, 0777, 100, '');
