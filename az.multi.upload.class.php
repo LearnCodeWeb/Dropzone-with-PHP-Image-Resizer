8535dbeaa13f110f4dbfc8e9a3ae63f0b44b911b
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2017-07-10 14:35:34 +0900

    Update az.multi.upload.class.php

--- az.multi.upload.class.php
+++ az.multi.upload.class.php
@@ -3,9 +3,51 @@ class ImageUploadAndResize{
 	
 	private $newWidth;
 	private $folderName;
-	public $n			=	0;
-	public $s			=	0;
-	public $Sflag		=	0;
+	public $n				=	0;
+	public $s				=	0;
+	public $Sflag			=	0;
+	public $prepareNames;
+	protected $isSubQuery 	= 	false;
+	
+	/**
+     * Database credentials
+     * @var string
+     */
+    protected $host;
+    protected $username;
+    protected $password;
+    protected $db;
+    protected $port;
+	
+	/**
+	* @param string $host
+	* @param string $username
+	* @param string $password
+	* @param string $db
+	* @param int $port
+	*/
+	
+	public function __construct($host = null, $username = null, $password = null, $db = null, $port = null)
+    {
+     	$mysqli = new mysqli($host, $username, $password, $db, $port);
+		/*
+		 * This is the "official" OO way to do it,
+		 * BUT $connect_error was broken until PHP 5.2.9 and 5.3.0.
+		 */
+		if ($mysqli->connect_error) {
+			die('Connect Error (' . $mysqli->connect_errno . ') '
+					. $mysqli->connect_error);
+		}
+		/*
+		 * Use this instead of $connect_error if you need to ensure
+		 * compatibility with PHP versions prior to 5.2.9 and 5.3.0.
+		 */
+		if (mysqli_connect_error()) {
+			die('Connect Error (' . mysqli_connect_errno() . ') '
+					. mysqli_connect_error());
+		}
+		$mysqli->close();   
+	}
 	
 	public function compressImage($sourceURL, $destinationURL, $minImgWidth, $wmImageSource="", $positionX="", $positionY="", $quality, $newWidth) {
 		$infoImg 	= 	getimagesize($sourceURL);
@@ -76,16 +118,18 @@ class ImageUploadAndResize{
 		return $fName;
 	}
 	
+	
 	public function uploadFiles($yourFileName, $yourDestination, $minImgWidth=400, $waterMarkImgSrc="", $xPosition="", $yPosition="", $reName="", $permission=0655, $quality=100, $newWidth=""){
 		if(!empty($_FILES[$yourFileName])){
 			foreach($_FILES[$yourFileName]['name'] as $val)
 			{
+				$infoExt 	= 	getimagesize($_FILES[$yourFileName]['tmp_name'][$this->n]);
 				$this->s++;
 				$filesName		=	str_replace(" ","",trim($_FILES[$yourFileName]['name'][$this->n]));
 				$files			=	explode(".",$filesName);
 				$File_Ext   	=   substr($_FILES[$yourFileName]['name'][$this->n], strrpos($_FILES[$yourFileName]['name'][$this->n],'.'));
 				
-				if($File_Ext==".png" || $File_Ext==".jpeg" || $File_Ext==".gif" || $File_Ext==".jpg")
+				if($infoExt['mime'] == 'image/gif' || $infoExt['mime'] == 'image/jpeg' || $infoExt['mime'] == 'image/png')
 				{
 					$srcPath	=	self::createDir($yourDestination, $permission).'/';
 					if($reName!=""){
@@ -95,8 +139,9 @@ class ImageUploadAndResize{
 					}
 					$path		=	trim($srcPath.$fileName);
 					if(self::compressImage($_FILES[$yourFileName]['tmp_name'][$this->n], $path, $minImgWidth, $waterMarkImgSrc, $xPosition, $yPosition, $quality, $newWidth))
-					{					
-						$this->Sflag	= 1; // success
+					{
+						$this->prepareNames[]	=	array($fileName); //need to be fixed.
+						$this->Sflag		= 	1; // success
 					}else{
 						$this->Sflag	= 2; // file not move to the destination
 					}
@@ -108,11 +153,12 @@ class ImageUploadAndResize{
 				$this->n++;
 			}
 			if($this->Sflag==1){
+				return $this->prepareNames;
 			echo '<div class="alert alert-success">Images uploaded successfully!</div>';
 			}else if($this->Sflag==2){
 				echo '<div class="alert alert-danger">File not move to the destination.</div>';
 			}else if($this->Sflag==3){
-				echo '<div class="alert alert-success">File extention not good.</div>';
+				echo '<div class="alert alert-danger">File extention not good. Try with <em>.PNG, .JPEG, .GIF, .JPG</em></div>';
 			}
 		}
 	}
