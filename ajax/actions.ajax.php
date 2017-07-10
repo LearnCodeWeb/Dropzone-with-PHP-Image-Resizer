58441a64aee3916526e65fdad7956341d04f8c66
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2017-07-10 14:44:59 +0900

    Update actions.ajax.php

--- ajax/actions.ajax.php
+++ ajax/actions.ajax.php
@@ -8,10 +8,3 @@ require('../az.multi.upload.class.php');
 $rename	=	rand(1000,5000).time();
 $upload	=	new ImageUploadAndResize('localhost','root','','test');
 $upload->uploadFiles('files', '../uploads', 250, '../mini-logo.png', 20, 20, $rename, 0777, 100, '');
-
-
-print"<pre>";
-foreach($upload->prepareNames as $name){
-	print_r($name[0]);
-}
-print"</pre>";
