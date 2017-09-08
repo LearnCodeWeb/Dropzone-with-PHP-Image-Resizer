0fc82d5fedb91a3fa75d2da6b8d208edcd57babf
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2017-09-08 11:13:39 +0900

    re move constructor

--- ajax/actions.ajax.php
+++ ajax/actions.ajax.php
@@ -6,5 +6,5 @@ ini_set('memory_limit','1000M');*/
 
 require('../az.multi.upload.class.php');
 $rename	=	rand(1000,5000).time();
-$upload	=	new ImageUploadAndResize('localhost','root','','test');
+$upload	=	new ImageUploadAndResize();
 $upload->uploadFiles('files', '../uploads', 250, '../mini-logo.png', 20, 20, $rename, 0777, 100, '');
